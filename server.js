d905ddc9292214ecbad63fb3dfb857aad87e14ef
Jakub Kozik <jakubkozik120@gmail.com>
2022-08-02 19:59:54 +0200

    Add Helmet

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const path = require('path');
 const socket = require('socket.io');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use('/api', testimonials);
