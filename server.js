d2cd457dc5059bf5a44190f574e528ed857d8a5e
Jakub Kozik <jakubkozik120@gmail.com>
2022-08-02 19:03:10 +0200

    change uir

--- server.js
+++ server.js
@@ -51,11 +51,11 @@ io.on('connection', (socket) => {
 const NODE_ENV = process.env.NODE_ENV;
 let dbUri = '';
 
-if (NODE_ENV === 'production') dbUri = 'url to remote db';
-else if (NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/companyDBtest';
-else
+if (NODE_ENV === 'production')
   dbUri =
     'mongodb+srv://jacob120:lp92rOiVYFyNnBUp@cluster0.2vwyi.mongodb.net/NewWaveDB';
+else if (NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/companyDBtest';
+else dbUri = 'mongodb://localhost:27017/NewWaveDB';
 
 mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
